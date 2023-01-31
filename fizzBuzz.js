339e2efeaf7dfadbe81f09daf299b1d92c7c4f81
Keadeish <you@example.com>
2023-01-31 09:08:45 +0000

    created repo

---
+++ fizzBuzz.js
@@ -0,0 +1,20 @@
+/*
+Loop 
+
+
+*/
+
+function fizzBuss(n) {
+    let outputArray = []
+
+    for (let i = 1; i <= n; i++) { //how can you use a for loop when it's only a number
+        //include readme file
+        //front-end or back-end -> which do you want. what even is BE lol
+        outputArray.push(i)
+        console.log(i)
+        console.log(outputArray)
+
+
+    }
+}
+fizzBuss(5)
